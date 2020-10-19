8a4b53c32b50d3943a97ed3256776cad786d983b
Radu <caracasradu11@gmail.com>
2020-10-19 21:11:33 +0300

    asdasd

---
+++ App/Classes/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace ConsoleApp1
+{
+    
+    class Program
+    {
+
+        public class Database
+        {
+            private static Database database = new Database();
+            List<object> tabel = new List<object>();
+            static int i = 0;
+
+            private Database()
+            {
+            }
+            public static Database Getdatabase()
+            {
+                return database;
+            }
+            public void AddEntry(object x)
+            {
+                database.tabel.Add(x);
+                i++;
+            }
+            
+
+        }
+
+        public class Users
+        {
+            string nume;
+            int varsta;
+            string sex;
+            public Users(string a,string b,int c)
+            {
+                nume = a;
+                sex = b;
+                varsta = c;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Users Radu = new Users("asad", "asda", 3);
+            Database db = Database.Getdatabase();
+            db.AddEntry(Radu);
+            Console.WriteLine(db);
+        }
+    }
+}
---
+++ App/Classes/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Reflection.Metadata;
+using System.Runtime.CompilerServices;
+
+namespace ConsoleApp2
+{
+    public sealed class Baza_de_date
+    {
+        
+        private static readonly Baza_de_date baza_de_date = new Baza_de_date();
+        private object[] tabele;
+        private Baza_de_date()
+        {
+        }
+        public static Baza_de_date GetBaza()
+        {
+            return Baza_de_date.baza_de_date;
+        }
+    public class User
+        {
+            string Nume;
+            int Varsta;
+            string Sex;
+            public User(string nume,int varsta,string sex)
+            {
+                Nume = nume;
+                Varsta = varsta;
+                Sex = sex;
+            }
+        }
+
+    }
+
+    private static void Main(string[] args)
+    {
+       
+    }
+
+
+
+    }
+}
